53cf35d89faa77af201cecd000f3f8941f555fab
Mr.Comet.Ha <niggen759@gmail.com>
2022-08-05 12:18:04 +0900

    2022.08.05 6강 완료

--- js/background.js
+++ js/background.js
@@ -1,4 +1,4 @@
-const img = ["00.jpg","01.jpg","02.jpg,","03.jpg","04.jpg"]
+const img = ["00.jpg","01.jpg","02.jpg","03.jpg","04.jpg"]
 
 const randImg = img[Math.floor(Math.random() * img.length)];
 
